a14a136068854ed84ec5c2cb4da7cc464fb59c79
Sandesh Shirse <shirse170906@gmail.com>
2026-02-19 20:26:52 +0530

    Add files via upload

---
+++ practice/listMeathods.py
@@ -0,0 +1,18 @@
+marks = [40, 50, 30, 10, 20]
+
+marks.append(60)
+print(marks)
+
+marks.sort()
+print(marks)
+
+print("\n")
+
+marks.insert(1,80)
+print(marks)
+
+marks.sort(reverse = True)
+print(marks)
+
+marks.reverse()
+print(marks)
