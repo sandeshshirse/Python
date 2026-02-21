4dc05ae0939b7a2c4f0da6eb84a8221037e6c830
Sandesh Shirse <shirse170906@gmail.com>
2026-02-21 19:08:44 +0530

    Add palindrome check for a list
    Check if a list is a palindrome and print the result.

---
+++ practice/palindrome.py
@@ -0,0 +1,13 @@
+list = ['a', 'abc', 's', 'abc', 'a']
+
+copy_list = list.copy()
+
+copy_list.reverse()
+
+if(copy_list == list):
+    print("yes it is a palindrome")
+
+else:
+    print("its not a palindrome")
+
+ 
