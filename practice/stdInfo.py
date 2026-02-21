7b66585735947abdc7e23fb5487683273e4c34ea
Sandesh Shirse <shirse170906@gmail.com>
2026-02-21 18:41:08 +0530

    Add files via upload

---
+++ practice/stdInfo.py
@@ -0,0 +1,11 @@
+m1 = input("Enter your name ")
+m2 = int(input("Enter your age " ))
+m3 = int(input("Enter id no. "))
+
+stdInfo = []
+
+stdInfo.append(m1)
+stdInfo.append(m2)
+stdInfo.append(m3)
+print(stdInfo)
+
