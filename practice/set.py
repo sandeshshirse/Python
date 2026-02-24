880e05d7e982d1c2c0193f2a5a1cb904f14fdd92
Sandesh Shirse <shirse170906@gmail.com>
2026-02-24 23:04:06 +0530

    Create set.py to illustrate set characteristics
    Add a new Python file to demonstrate set properties.

---
+++ practice/set.py
@@ -0,0 +1,14 @@
+#  sets are the collaction of the unorderd items
+#  immutable
+#  unique
+#  repeated elements only stored once
+#  set = { }
+
+set = {1, 2, 2, 3, 4, 4, 5}
+
+print(set)
+print(type(set))
+print(len(set))
+
+collaction = set()
+print(type(collaction))
