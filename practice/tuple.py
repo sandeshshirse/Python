9a00c06c76a8b4fa8f51958f312c355af585636e
Sandesh Shirse <shirse170906@gmail.com>
2026-02-28 21:09:28 -0800

    Add files via upload

---
+++ practice/tuple.py
@@ -0,0 +1,9 @@
+#tuples   they are immutable
+
+tup = (1, 2, 3, 4, 5, 6, 2,)   #if we dont give coma then system treate it as a variable
+print(tup)   #so " , " is compulsory at the end of the tuple
+
+
+print(tup.index(2))   #index of the first occurance
+
+print(tup.count(2))   #counts of total occurance
