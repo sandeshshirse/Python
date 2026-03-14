f8d2f96a5d44d8fdbf49b6ff3e13c002ff123154
Sandesh Shirse <shirse170906@gmail.com>
2026-03-14 22:47:29 +0530

    Add grade calculation based on percentage input

---
+++ practice/grade.py
@@ -0,0 +1,13 @@
+per = float(input("Enter Percentage: "))
+if per >= 75:
+    print("Grade: Distinction")
+elif per >= 60:
+    print("Grade: First Class")
+elif per >= 50:
+    print("Grade: Second Class")
+elif per >= 40:
+    print("Grade: Pass")
+else:
+    print("Grade: Fail")
+Enter Percentage: 95
+Grade: Distinction
